9aede32014555f7f8e529961716d5f1ad3db9865
Shivansh Thakur <56513036+Shivansh-Thakur@users.noreply.github.com>
2021-10-22 22:58:48 +0530

    Add files via upload

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/careshop/admin/');
+define('HTTP_CATALOG', 'http://localhost/careshop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/careshop/admin/');
+define('HTTPS_CATALOG', 'http://localhost/careshop/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/careshop/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/careshop/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/careshop/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/careshop/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/careshop/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/careshop/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/careshop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/careshop/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/careshop/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/careshop/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/careshop/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/careshop/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'careshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
